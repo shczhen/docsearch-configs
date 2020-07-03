047fbb099d58e54a9694e3ef836f68109a65cb22
YiniXu9506 <xuyini@pingcap.com>
2020-07-03 17:53:22 +0800

    chore: update algolia for new docs website

--- crawl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-folder=(en-stable en-dev en-v2.1 en-v3.1 en-v3.0 en-v1.0)
-
-for v in "${folder[@]}"
-do
-  echo "$v"
-  docker run --env-file=.env -e "CONFIG=$(cat /Users/yinixu/pingcap/docsearch-configs/$v/$v.json | jq -r tostring)" algolia/docsearch-scraper &
-done
